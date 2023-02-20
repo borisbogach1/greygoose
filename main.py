8ff0d1774d5369a8116eacf4080620d0bf3ce014
Fedor Zverev <fdzverev12@gmail.com>
2023-02-20 23:32:08 +0300

    Added an oppotunity to create a route consisting of 5 points

--- main.py
+++ main.py
@@ -62,7 +62,17 @@ def creareroute():
         # point13=get_key(d,point2+"\n")
         # print(point11,point12,point13)
 
-        return str(routepoint)
+
+        file='route_description/11.txt'
+        file=open(file,mode='w')
+        for i in routepoint:
+            file.write(i+'\t#'+'\n')
+        create_map('route_description/11.txt',spn='0.01,0.01')
+        values = {"number": '11',
+                  "file_map": "../static/" + '11' + ".png"}
+        return render_template("suggested_route.html", **values)
+
+        return
     else:
         return render_template("createroute.html", form=form)
 
