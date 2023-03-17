6f29ab1cb7e9364db176902e81929f975ee6869d
Elizaveta Knyazeva <Liza.knyazeva62@gmail.com>
2023-03-17 13:31:29 +0300

    New design for our WEB-site

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route("/home")
 def index():
-    return render_template("index.html")
+    return render_template("main_page.html")
 
 
 @app.route('/interestpoints')
